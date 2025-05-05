9ddf5e29a1d5854531ba88871e396a403f21cac3
Hadi493 <hadialam493@gmail.com>
2025-05-05 16:33:57 +0600

    initial commit

---
+++ chapter-one.py
@@ -0,0 +1,7 @@
+import streamlit as st # type: ignore
+
+st.title("Hello from Streamlit!")
+st.write("This is a simple Streamlit app.")
+st.markdown("## Markdown Example")
+st.code("print('Hello World!')")
+st.button("Click me!")
